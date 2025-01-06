cef93e1bed270146e943392793bf1f2b037ee154
Maxim Kvazarov <152982801+0xMe1ok@users.noreply.github.com>
2025-01-06 15:34:39 +0700

    Removed link

--- 3_gp_action.ino
+++ 3_gp_action.ino
@@ -94,9 +94,6 @@ void action() {
       nextBellTime = getNextBellTime();
       timeUntilBell = getTimeUntilBell();
     }
-
-    // Иначе - сохранение из поля ввода в переменную
-    // https://github.com/GyverLibs/GyverPortal/wiki/%D0%9E%D0%B1%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%BA%D0%B0-%D0%BA%D0%BB%D0%B8%D0%BA%D0%BE%D0%B2
     // Сохранение значения для файла Config
     if (ui.clickSub("CFG")) {
       if (ui.clickNameSub(1) == "STR") {
