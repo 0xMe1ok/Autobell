608ebaa737cb291a80a700dbc42a5b9bd950df5a
Maxim Kvazarov <152982801+0xMe1ok@users.noreply.github.com>
2025-01-12 13:17:48 +0700

    Corrected wifif connection time

--- 0_setup.ino
+++ 0_setup.ino
@@ -1,6 +1,7 @@
 void setup() {
   // Выставление режима пина для реле и кнопки
   pinMode(RELAY_PIN, OUTPUT);
+  pinMode(RESET_BUTTON_PIN, INPUT_PULLUP);
 
   // Запуск часов реального времени и экрана
   Wire.begin(CLOCK_SDA, CLOCK_SCL); // SDA, SCL
@@ -31,6 +32,8 @@ void setup() {
   timeUntilBell = getTimeUntilBell();
   currentDay = ds.day();
 
+  const int preparationTime = millis();
+
   // Подключение к WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(configFile[SH("WiFi_SSID")], configFile[SH("WiFi_PASS")]);
@@ -38,7 +41,7 @@ void setup() {
     delay(500);
     
     // Если время подключения превышено...
-    if (millis() > wifiConnectionAwait) {
+    if (millis() > wifiConnectionAwait + preparationTime) {
       // то создаем точку доступа
       WiFi.mode(WIFI_AP);
       WiFi.softAP("Autobell");
